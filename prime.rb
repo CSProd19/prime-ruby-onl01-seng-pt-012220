fd2863a2b63d2a778b80f094caa3fac727e65b54
Maria Connie Sanchez <msnchz04@gmail.com>
2020-02-04 03:41:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+
+
+
+#(1..10).to_a => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def prime?(number)
+  if number < 2 
+    return false 
+  else 
+    (2..number -1).to_a.all? do |n|
+      number % n != 0 
+    end 
+  end 
+end 
