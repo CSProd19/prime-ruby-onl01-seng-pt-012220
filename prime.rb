d613256758aafd76c22e408311b33ba4c12c79e9
Maria Connie Sanchez <msnchz04@gmail.com>
2020-02-04 03:35:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,6 @@ What defines an integer as a prime number? Research algorithms for how to determ
 How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
 
 
-
 #(1..10).to_a => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def prime?(number)
